17b4c470edd8eca804c8042e721ad25bb363e070
Sarah Elkhart <selkhart@mail.greenriver.edu>
2018-01-19 22:55:09 -0500

    final adjustments

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
+//Sarah Elkhart
+//17 January 2018
+//index.php
+
 //require the autoload file
 require_once('vendor/autoload.php');
 
 //create an instance of the base class
 $f3 = Base::instance();
 
+//set debug (error reporting) level
+$f3->set('DEBUG',3);
 
 //set default route to home.html
 $f3->route('GET /', function(){
